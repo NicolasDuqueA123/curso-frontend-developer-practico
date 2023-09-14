dc01b60934e8e7bed312eff30fd24658d92dc10a
Nicodq199101~ <nicoduque1821@hotmail.com>
2023-09-13 23:55:21 -0500

    commit version 130923

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 //forma 1, completa
 
@@ -19,7 +23,44 @@ const desktopMenu = document.querySelector('.desktop-menu');
 //esta seria la mejor practica porque si se debe remover el evento al ser anonimo no se puede hacer, en este caso primero se debe inicializar la funcion por hoisting
 
 const toggleDesktopMenu = () => {
+  //desktopMenu.classList.toggle("inactive");
+  const isAsideMenuClosed = aside.classList.contains('inactive');
+
+  if(!isAsideMenuClosed){
+    aside.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
+
+
 };
 
 navEmail.addEventListener("click", toggleDesktopMenu);
+
+const toggleMobileMenu = () => {
+  //mobileMenu.classList.toggle("inactive");
+
+  const isAsideMenuClosed = aside.classList.contains('inactive');
+
+  if(!isAsideMenuClosed){
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+};
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+//funcion para que los menus no se sobrepongan
+//no se realiza con arrow function para que sea como en la clase
+
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
+    aside.classList.toggle("inactive");   
+}
+
+
+
+
+
